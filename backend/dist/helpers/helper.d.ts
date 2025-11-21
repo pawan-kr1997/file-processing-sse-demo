a04fc089a9d11950a761a60d59fce843b0e60023
Pawan Kumar <pawankumar@Pawans-MacBook-Air.local>
2025-11-21 08:08:43 +0530

    Remove node_modules and dist from version control

--- backend/dist/helpers/helper.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const validateFile: () => Promise<unknown>;
-export declare const convertFile: () => Promise<unknown>;
-export declare const extractContent: () => Promise<unknown>;
-export declare const processSemantic: () => Promise<unknown>;
-export declare const generateSpeechScript: () => Promise<unknown>;
-//# sourceMappingURL=helper.d.ts.map
